3437504c3d6b187d3888f5823e1904474d793818
Ayush Rawat <rwtayush@Ayushs-MacBook-Air.local>
2026-05-03 03:52:51 +0530

    Implementing the thread class

---
+++ Multithreading/MyRunnable.java
@@ -0,0 +1,11 @@
+class MyRunnable implements Runnable{
+
+    // run method is the entry point for the thread when it is started
+    // it contain the code that will be executed in the new thread
+    @Override
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println(Thread.currentThread()+ " , "+i);
+        }
+    }
+}
